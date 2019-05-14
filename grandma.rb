04938108acc3bdfdcd85df1f760f783814ecaedd
Angela Lo <angelawenlo@gmail.com>
2019-05-14 14:00:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,10 @@
 require 'pry'
 def speak_to_grandma(response)
   if response == response.upcase
+      binding.pry
     "I LOVE YOU TOO PUMPKIN!"
   elsif response == response.downcase
     "HUH?! SPEAK UP, SONNY!"
-  binding.pry
+
   end
 end
