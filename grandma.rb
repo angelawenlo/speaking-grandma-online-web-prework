bd589aeecd444c78911429c17412f8678d8aae42
Angela Lo <angelawenlo@gmail.com>
2019-05-14 14:04:02 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,9 +2,8 @@ require 'pry'
 def speak_to_grandma(response)
   if response == response.upcase
     "I LOVE YOU TOO PUMPKIN!"
-  elsif response == response.downcase
-
-
+  elsif response == response.upcase
+    "NO, NOT SINCE 1938!"
   else
     "HUH?! SPEAK UP, SONNY!"
   end
