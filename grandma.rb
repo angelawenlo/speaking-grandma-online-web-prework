a60b9b2dc123b799e8f767da79c985a016b14230
Angela Lo <angelawenlo@gmail.com>
2019-05-14 13:54:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,8 @@
 def speak_to_grandma(response)
   if response.uppercase
-    puts "I LOVE YOU TOO PUMPKIN!""
+    puts "I LOVE YOU TOO PUMPKIN!"
+  elsif condition
+
 
   else
     puts "HUH?! SPEAK UP, SONNY!"
